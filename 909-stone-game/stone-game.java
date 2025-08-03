9217f65f152bb274e29af0f0fd843bf00fc52189
bharathm2004 <125333644+bharathm2004@users.noreply.github.com>
2025-08-03 22:46:52 +0530

    Time: 1 ms (39.98%) | Memory: 44.3 MB (31.07%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        // dp will store the maximum difference of stone while playing betweem i & j pile
+        int dp[][]= new int[n][n];
+        return true;
+
+
+    }
+}
